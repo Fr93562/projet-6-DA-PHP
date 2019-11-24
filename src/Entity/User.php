483deaa65411d29b682fd81b083b6e139c5fe548
Fr93562 <52503874+Fr93562@users.noreply.github.com>
2019-11-24 18:28:03 +0100

    Add files via upload
    ajout des éléments lié à la sécurité

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,17 +2,13 @@
 
 namespace App\Entity;
 
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-
-use Service\Tools\Slugger;
-
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  */
-class User 
+class User implements UserInterface
 {
     /**
      * @ORM\Id()
@@ -22,31 +18,25 @@ class User
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=180, unique=true)
      */
     private $username;
 
-    private $slug;
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $roles = [];
+
+    /**
+     * @var string The hashed password
+     * @ORM\Column(type="string")
+     */
+    private $password;
 
     /**
      * @ORM\Column(type="string", length=255)
      */
     private $mail;
-
-	/**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $password;
-
-	/**
-   	* @ORM\Column(name="salt", type="string", length=255, nullable=true, options={"default" : "default"})
-   	*/
-  	private $salt;
-
-  	/**
-   	* @ORM\Column(name="roles", type="string", length=100, nullable=true, options={"default" : "User"})
-   	*/
-  	private $roles;
 
     /**
      * @ORM\OneToMany(targetEntity="App\Entity\Comment", mappedBy="username", orphanRemoval=true)
@@ -54,19 +44,19 @@ class User
 
     private $comments;
 
-    public function __construct()
-    {
-        $this->comments = new ArrayCollection();
-    }
-
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getUsername(): ?string
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUsername(): string
     {
-        return $this->username;
+        return (string) $this->username;
     }
 
     public function setUsername(string $username): self
@@ -76,16 +66,55 @@ class User
         return $this;
     }
 
-    public function getSlug(): ?string
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
     {
-        return $this->slug;
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
     }
 
-    public function setSlug(string $titre): self
+    public function setRoles(array $roles): self
     {
-        $this->slug = Slugger::slugify($titre);
+        $this->roles = $roles;
 
         return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getPassword(): string
+    {
+        return (string) $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getSalt()
+    {
+        // not needed when using the "bcrypt" algorithm in security.yaml
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
     }
 
     public function getMail(): ?string
@@ -96,42 +125,6 @@ class User
     public function setMail(string $mail): self
     {
         $this->mail = $mail;
-
-        return $this;
-    }
-
-	public function getPassword(): ?string
-    {
-        return $this->password;
-    }
-
-    public function setPassword(string $password): self
-    {
-        $this->password = $password;
-
-        return $this;
-    }
-
-	public function getSalt(): ?string
-    {
-        return $this->salt;
-    }
-
-    public function setSalt(string $salt): self
-    {
-        $this->salt = $salt;
-
-        return $this;
-    }
-
-	public function getRoles(): ?string
-    {
-        return $this->roles;
-    }
-
-    public function setRoles(string $roles): self
-    {
-        $this->roles = $roles;
 
         return $this;
     }
